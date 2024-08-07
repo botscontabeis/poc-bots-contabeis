07d0bbae0f567e91502dd2406ad7597377c9bcc5
Elder Santos <elder_rn@hotmail.com>
2024-08-07 00:00:21 -0300

    feat: add management command to create super user automagically

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,10 @@ PROCESS_TYPE=$1
 if [ "$PROCESS_TYPE" = "django" ]; then
   python manage.py makemigrations
   python manage.py migrate
+  python manage.py createsuperuser_if_none_exists \
+    --username=$ADMIN_USERNAME \
+    --password=$ADMIN_PASSWORD \
+    --email=$ADMIN_EMAIL
 
   if [ "$DEBUG" = "True" ]; then
     python manage.py runserver 0.0.0.0:$PORT
