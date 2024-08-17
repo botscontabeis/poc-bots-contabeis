0a1c9ee72d678df96323fbea138fefe6f1d54dca
Elder Santos <elder_rn@hotmail.com>
2024-08-17 12:26:43 -0300

    chore: add TODO in credentials.py

--- bots/credentials.py
+++ bots/credentials.py
@@ -1,4 +1,9 @@
 from decouple import config
 
+"""
+TODO: criar modelo de credenciais de modo que seja possível:
+- atualizar as credenciais sem precisar alterar o código
+- ter mais de um conjunto de credenciais por aplicação (?)
+"""
 UVT_USERNAME = config("UVT_USERNAME")
 UVT_PASSWORD = config("UVT_PASSWORD")
