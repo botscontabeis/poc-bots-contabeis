da18bf296348656e135f257c2dd672da3aa45a0e
Elder Santos <elder_rn@hotmail.com>
2024-08-19 22:16:50 -0300

    feat: add task_postrun signal to gracefully shutdown worker

--- core/tasks.py
+++ core/tasks.py
@@ -1,6 +1,13 @@
-from celery import shared_task
+from celery import signals, shared_task
+from celery.exceptions import WorkerShutdown
 
 
 @shared_task
 def debug_task():
     return "Debug task running successfully!"
+
+
+@signals.task_postrun.connect
+def shutdown_worker(**kwargs):
+    print("Tarefa concluída, finalizando o worker suavemente...")
+    raise WorkerShutdown()
